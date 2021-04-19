334339bff9da00cbd895e4e73b608ab1013a2eca
Juli10s <82818895+Juli10s@users.noreply.github.com>
2021-04-19 16:50:10 +0200

    Update PersistenceConfiguration.java

--- src/main/java/com/pluralsight/conference2/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conference2/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-        public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.username("root");
-        builder.password("juli0s");
-        builder.url("jdbc:mysql://localhost:3306/conference_demo");
-        System.out.println("Datasource im");
-        return builder.build();
-        }
+//     @Bean
+//         public DataSource dataSource(){
+//         DataSourceBuilder builder = DataSourceBuilder.create();
+//         builder.username("root");
+//         builder.password("juli0s");
+//         builder.url("jdbc:mysql://localhost:3306/conference_demo");
+//         System.out.println("Datasource im");
+//         return builder.build();
+//         }
 }
