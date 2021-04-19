f54556ad9626be47d1c66fc790a5afe27528a5c4
Juli10s <82818895+Juli10s@users.noreply.github.com>
2021-04-19 15:26:28 +0200

    Update Speaker.java

--- src/main/java/com/pluralsight/conference2/models/Speaker.java
+++ src/main/java/com/pluralsight/conference2/models/Speaker.java
@@ -2,7 +2,6 @@ package com.pluralsight.conference2.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.sun.jdi.PrimitiveValue;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
